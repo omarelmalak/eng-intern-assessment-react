7ce7615070c7e5f738524d3e1e5aa2bd80d30c32
omarelmalak28 <omar.elmalak@mail.utoronto.ca>
2024-01-29 01:33:32 -0500

    STO4: Implemented laps list w/ updating best/worst

--- src/components/StopWatch.tsx
+++ src/components/StopWatch.tsx
@@ -35,12 +35,6 @@ export default function StopWatch() {
 
     const [currInterval, setCurrInterval] = useState(null);
 
-    const handleReset = (reset: boolean) => {
-        setLapReset(false);
-        setStartTime(null);
-        setDiff(0);
-    }
-
     useEffect(() => {
         if (lapReset) {
             setStartTime(Date.now() - diff);
@@ -60,12 +54,68 @@ export default function StopWatch() {
     const hr = (Math.floor(diff / (1000 * 60 * 60)) < 10) ? ("0" + Math.floor(diff / (1000 * 60 * 60))) : (Math.floor(diff / (1000 * 60 * 60)));
     const min = (Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)) < 10) ? ("0" + Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))) : (Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)));
     const sec = (Math.floor((diff % (1000 * 60)) / 1000) < 10) ? ("0" + Math.floor((diff % (1000 * 60)) / 1000)) : (Math.floor((diff % (1000 * 60)) / 1000));
-    const milli = (diff % 1000 < 10) ? (("00" + (diff % 1000))) : ((diff % 1000 < 100)) ? (("0" + (diff % 1000))) : ((((diff % 1000))))
+    const milli = (diff % 1000 < 10) ? (("00" + (diff % 1000))) : ((diff % 1000 < 100)) ? (("0" + (diff % 1000))) : ((((diff % 1000))));
 
+    const [laps, setLaps] = useState<string[]>([]);
+    const [lapsRaw, setLapsRaw] = useState<number[]>([]);
+
+    const [maxLapDiff, setMaxLapDiff] = useState(0);
+    const [maxLapDiffIndex, setMaxLapDiffIndex] = useState(0);
+
+    const [minLapDiff, setMinLapDiff] = useState(0);
+    const [minLapDiffIndex, setMinLapDiffIndex] = useState(0);
+
+    const handleReset = (resetMode: boolean) => {
+
+        if (resetMode) {
+            setLapsRaw(prevLapsRaw => [...prevLapsRaw, diff]);
+
+            if (lapsRaw.length >= 1) {
+                const lapDiff = diff - lapsRaw[lapsRaw.length - 1]
+
+                const compareHr = (Math.floor(lapDiff / (1000 * 60 * 60)) < 10) ? ("0" + Math.floor(lapDiff / (1000 * 60 * 60))) : (Math.floor(lapDiff / (1000 * 60 * 60)));
+                const compareMin = (Math.floor((lapDiff % (1000 * 60 * 60)) / (1000 * 60)) < 10) ? ("0" + Math.floor((lapDiff % (1000 * 60 * 60)) / (1000 * 60))) : (Math.floor((lapDiff % (1000 * 60 * 60)) / (1000 * 60)));
+                const compareSec = (Math.floor((lapDiff % (1000 * 60)) / 1000) < 10) ? ("0" + Math.floor((lapDiff % (1000 * 60)) / 1000)) : (Math.floor((lapDiff % (1000 * 60)) / 1000));
+                const compareMilli = (lapDiff % 1000 < 10) ? (("00" + (lapDiff % 1000))) : ((lapDiff % 1000 < 100)) ? (("0" + (lapDiff % 1000))) : ((((lapDiff % 1000))));
+
+                if (lapDiff >= maxLapDiff) {
+                    setMaxLapDiff(lapDiff)
+                    setMaxLapDiffIndex(lapsRaw.length)
+                    console.log("NEW MAX: ", maxLapDiffIndex)
+                } else if (lapDiff <= minLapDiff) {
+                    setMinLapDiff(lapDiff)
+                    setMinLapDiffIndex(lapsRaw.length)
+                    console.log("NEW MIN: ", maxLapDiffIndex)
+                }
+
+
+
+
+                setLaps(prevLaps => [...prevLaps, compareHr + ':' + compareMin + ":" + compareSec + "." + compareMilli]);
+            } else {
+                setMinLapDiff(diff);
+                setMaxLapDiff(diff);
+                setLaps(prevLaps => [...prevLaps, hr + ':' + min + ":" + sec + "." + milli]);
+            }
+
+        } else {
+            setStartTime(null);
+            setDiff(0);
+            setLaps([]);
+            setLapsRaw([]);
+
+            setMaxLapDiff(Number.MIN_SAFE_INTEGER);
+            setMaxLapDiffIndex(0);
+
+            setMinLapDiff(Number.MAX_SAFE_INTEGER);
+            setMinLapDiffIndex(0);
+        }
+
+    }
 
     return (
         <div className="container text-center" style={currentMode}>
-            <h1 style={{ fontSize: '100px', fontWeight: 'bold', padding: '20px' }}>
+            <h1 style={{ fontSize: '7.5vw', fontWeight: 'bold', padding: '20px' }}>
                 {hr}:{min}:{sec}.{milli}
             </h1>
 
@@ -76,7 +126,19 @@ export default function StopWatch() {
                 <StartStopButton setLapResetButtonType={handleStartStop} ></StartStopButton>
                 <div className="container" style={{ width: '1px' }}></div>
             </div>
-
+            <div className="col" style={{ height: '5vh', paddingTop: '6vh', paddingBottom: '3vh', backgroundColor: '' }}>
+                <div className="container" style={{ maxWidth: '50vw', height: '3px', padding: '', backgroundColor: 'grey', borderRadius: '20px' }}></div>
+            </div>
+            <ul className="list-group" style={{ background: '', borderColor: '', height: '' }}>
+                {laps.map((lap, index) =>
+                    <li className="list-group-item"
+                        key={lap}
+                        onClick={() => console.log(lap)}
+                        style={{ background: '', borderColor: 'rgba(150, 191, 72, 0.4)', borderBlockEndColor: '', height: '10vh', fontSize: '50px', backgroundColor: (laps.length <= 2) ? ('transparent') : (index == maxLapDiffIndex) ? ('red') : (index == minLapDiffIndex) ? ('green') : 'transparent' }}
+                    >
+                        Lap {index + 1}: {lap}
+                    </li>)}
+            </ul>
 
 
         </div>
